711404abd3f6a38cc158f5e02ea3f8431b30e7e8
striderjg <JohnG27@yahoo.com>
2025-01-11 23:30:53 -0800

    added Player Class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -14,13 +15,20 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
+    clock = pygame.time.Clock()
+    dt  = 0
     while(True):
         for event in pygame.event.get():
             if(event.type == pygame.QUIT):
                 return
 
         screen.fill(0)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
     
     
 if __name__ == "__main__":
